8c1c298a2efc85315383a6e94af54bcc2f8a1bd1
Christian <matschke.berlin@icloud.com>
2021-07-29 13:27:40 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,5 +38,16 @@ publishing {
 
             from(components["java"])
         }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/0OZ/NanoIdKt")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
     }
 }
